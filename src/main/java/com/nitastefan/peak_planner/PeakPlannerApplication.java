da6093a4c51f8a3315d71c16da00f20454b77f29
sn <scarsan62@gmail.com>
2024-11-03 18:31:21 +0200

    initializer commit

---
+++ src/main/java/com/nitastefan/peak_planner/PeakPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.nitastefan.peak_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeakPlannerApplication.class, args);
+	}
+
+}
